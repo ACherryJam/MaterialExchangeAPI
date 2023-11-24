dd03a46efb573406a9bfc0e39ad9d0bfa9d4c286
Alexander Starikov <acherryjam@gmail.com>
2023-11-25 00:41:10 +0300

    feat: Material and Seller models

---
+++ MaterialExchangeAPI/Models/Material.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaterialExchangeAPI.Models
+{
+    public class Material
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+
+        [ForeignKey("Seller")]
+        public int SellerId { get; set; }
+        public Seller Seller { get; set; }
+    }
+}
