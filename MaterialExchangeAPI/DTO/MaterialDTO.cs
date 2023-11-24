5929a1b649fadb94a4da19a2ede2dfd7249560ab
Alexander Starikov <acherryjam@gmail.com>
2023-11-25 00:48:23 +0300

    feat: Data-transer-objects for Material and Seller API calls

---
+++ MaterialExchangeAPI/DTO/MaterialDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace MaterialExchangeAPI.DTO
+{
+    public class GetMaterialDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int SellerId { get; set; }
+    }
+
+    public class CreateMaterialDTO
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int SellerId { get; set; }
+    }
+
+    // Id is passed as an URL argument
+    public class UpdateMaterialDTO
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int SellerId { get; set; }
+    }
+}
---
+++ MaterialExchangeAPI/DTO/SellerDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace MaterialExchangeAPI.DTO
+{
+    public class GetSellerDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class CreateSellerDTO
+    {
+        public string? Name { get; set; }
+    }
+
+    // Id is passed as an URL argument
+    public class UpdateSellerDTO
+    {
+        public string? Name { get; set; }
+    }
+}
