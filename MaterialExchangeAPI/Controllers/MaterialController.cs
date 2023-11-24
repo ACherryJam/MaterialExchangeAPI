c4718cf23f0f5e7351c6c84754eff0037d2a59d2
Alexander Starikov <acherryjam@gmail.com>
2023-11-25 00:52:42 +0300

    feat: empty controllers for Material and Seller

---
+++ MaterialExchangeAPI/Controllers/MaterialController.cs
@@ -0,0 +1,47 @@
+﻿using MaterialExchangeAPI.DTO;
+using MaterialExchangeAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialExchangeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/materials")]
+    public class MaterialController : ControllerBase
+    {
+        public MaterialController()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetMaterials()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult> GetMaterialById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddMaterial(CreateMaterialDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMaterial(int id, UpdateMaterialDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteMaterial(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MaterialExchangeAPI/Controllers/SellerController.cs
@@ -0,0 +1,45 @@
+﻿using MaterialExchangeAPI.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialExchangeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/sellers")]
+    public class SellerController : ControllerBase
+    {
+        public SellerController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetSellers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult> GetSellerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddSeller(CreateSellerDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateSeller(int id, UpdateSellerDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteSeller(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
