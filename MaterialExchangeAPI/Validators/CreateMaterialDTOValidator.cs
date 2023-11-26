a6113ba3679ed101c386d937c0cf85cda335cdec
Alexander Starikov <acherryjam@gmail.com>
2023-11-26 16:04:13 +0300

    change: added SellerId existence check in Material validators

--- MaterialExchangeAPI/Validators/CreateMaterialDTOValidator.cs
+++ MaterialExchangeAPI/Validators/CreateMaterialDTOValidator.cs
@@ -1,14 +1,24 @@
 ﻿using FluentValidation;
+using MaterialExchangeAPI.Data.Repositories;
 using MaterialExchangeAPI.DTO;
 
 namespace MaterialExchangeAPI.Validators
 {
     public class CreateMaterialDTOValidator : AbstractValidator<CreateMaterialDTO>
     {
-        public CreateMaterialDTOValidator() 
+        public CreateMaterialDTOValidator(ISellerRepository repository) 
         {
             RuleFor(material => material.Name).NotEmpty();
             RuleFor(material => material.Price).GreaterThan(0);
+            RuleFor(material => material.SellerId).Custom(
+                (id, context) =>
+                {
+                    if (!repository.Exists(id))
+                    {
+                        context.AddFailure("SellerID must exist");
+                    }
+                }
+            );
         }
     }
 }
--- MaterialExchangeAPI/Validators/UpdateMaterialDTOValidator.cs
+++ MaterialExchangeAPI/Validators/UpdateMaterialDTOValidator.cs
@@ -1,14 +1,24 @@
 ﻿using FluentValidation;
+using MaterialExchangeAPI.Data.Repositories;
 using MaterialExchangeAPI.DTO;
 
 namespace MaterialExchangeAPI.Validators
 {
     public class UpdateMaterialDTOValidator : AbstractValidator<UpdateMaterialDTO>
     {
-        public UpdateMaterialDTOValidator() 
+        public UpdateMaterialDTOValidator(ISellerRepository repository) 
         {
             RuleFor(material => material.Name).NotEmpty();
             RuleFor(material => material.Price).GreaterThan(0);
+            RuleFor(material => material.SellerId).Custom(
+                (id, context) =>
+                {
+                    if (!repository.Exists(id))
+                    {
+                        context.AddFailure("SellerID must exist");
+                    }
+                }
+            );
         }
     }
 }
