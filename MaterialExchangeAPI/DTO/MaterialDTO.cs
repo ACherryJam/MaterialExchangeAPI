65bd0c7091f6be0657aeebcf7fab9f17e1acf1e0
Alexander Starikov <acherryjam@gmail.com>
2023-11-26 12:58:54 +0300

    docs: DTO documentation

--- MaterialExchangeAPI/DTO/MaterialDTO.cs
+++ MaterialExchangeAPI/DTO/MaterialDTO.cs
@@ -2,28 +2,83 @@
 
 namespace MaterialExchangeAPI.DTO
 {
+    /// <summary>
+    /// Формат получения данных о материале
+    /// </summary>
     public class GetMaterialDTO
     {
+        /// <summary>
+        /// ID материала
+        /// </summary>
+        /// <example>1</example>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Имя материала
+        /// </summary>
+        /// <example>Кирпич</example>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// Цена продажи материала
+        /// </summary>
+        /// <example>42</example>
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// ID продавца материала
+        /// </summary>
+        /// <example>1</example>
         public int? SellerId { get; set; } = null;
     }
 
+    /// <summary>
+    /// Формат создания материала
+    /// </summary>
     public class CreateMaterialDTO
     {
+        /// <summary>
+        /// Имя материала
+        /// </summary>
+        /// <example>Кирпич</example>
         [Required]
         public string? Name { get; set; }
+
+        /// <summary>
+        /// Цена продажи материала
+        /// </summary>
+        /// <example>42</example>
         [Required]
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// ID продавца материала
+        /// </summary>
+        /// <example>1</example>
         public int? SellerId { get; set; } = null;
     }
 
-    // Id is passed as an URL argument
+    /// <summary>
+    /// Формат изменения данных о материале
+    /// </summary>
     public class UpdateMaterialDTO
     {
+        /// <summary>
+        /// Имя материала
+        /// </summary>
+        /// <example>Бетон</example>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// Цена продажи материала
+        /// </summary>
+        /// <example>43</example>
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// ID продавца материала
+        /// </summary>
+        /// <example>2</example>
         public int? SellerId { get; set; } = null;
     }
 }
--- MaterialExchangeAPI/DTO/SellerDTO.cs
+++ MaterialExchangeAPI/DTO/SellerDTO.cs
@@ -2,21 +2,46 @@
 
 namespace MaterialExchangeAPI.DTO
 {
+    /// <summary>
+    /// Формат получения данных о продавце
+    /// </summary>
     public class GetSellerDTO
     {
+        /// <summary>
+        /// ID продавца
+        /// </summary>
+        /// <example>1</example>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Имя продавца
+        /// </summary>
+        /// <example>Олег</example>
         public string? Name { get; set; }
     }
 
+    /// <summary>
+    /// Формат создания продавца
+    /// </summary>
     public class CreateSellerDTO
     {
+        /// <summary>
+        /// Имя продавца
+        /// </summary>
+        /// <example>Олег</example>
         [Required]
         public string? Name { get; set; }
     }
 
-    // Id is passed as an URL argument
+    /// <summary>
+    /// Формат изменения данных о материале
+    /// </summary>
     public class UpdateSellerDTO
     {
+        /// <summary>
+        /// Имя продавца
+        /// </summary>
+        /// <example>Артём</example>
         public string? Name { get; set; }
     }
 }
